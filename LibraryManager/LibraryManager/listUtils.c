1b3eb0981b88ad83753501e6f0c6694b3d8ff6a2
David Hein <david-hein196@web.de>
2019-01-28 14:10:31 +0100

    Add listUtils.c/h - file for printing search results

---
+++ LibraryManager/LibraryManager/listUtils.c
@@ -0,0 +1,9 @@
+//
+//  listUtils.c
+//  LibraryManager
+//
+//  Created by David Hein & Felix Hieser on 28.01.19.
+//  Copyright © 2019 David Hein & Felix Hieser. All rights reserved.
+//
+
+#include "listUtils.h"
---
+++ LibraryManager/LibraryManager/listUtils.h
@@ -0,0 +1,14 @@
+//
+//  listUtils.h
+//  LibraryManager
+//
+//  Created by David Hein & Felix Hieser on 28.01.19.
+//  Copyright © 2019 David Hein & Felix Hieser. All rights reserved.
+//
+
+#ifndef listUtils_h
+#define listUtils_h
+
+#include <stdio.h>
+
+#endif /* listUtils_h */
