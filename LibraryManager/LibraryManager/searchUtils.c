2e7e6a8066e63a5e477aca1574e8acf59c119d87
David Hein <david-hein196@web.de>
2019-01-28 14:12:06 +0100

    Add searchUtils.c/h - file for all search methods like levenshtein distance

---
+++ LibraryManager/LibraryManager/searchUtils.c
@@ -0,0 +1,9 @@
+//
+//  searchUtils.c
+//  LibraryManager
+//
+//  Created by David Hein & Felix Hieser on 28.01.19.
+//  Copyright © 2019 David Hein & Felix Hieser. All rights reserved.
+//
+
+#include "searchUtils.h"
---
+++ LibraryManager/LibraryManager/searchUtils.h
@@ -0,0 +1,14 @@
+//
+//  searchUtils.h
+//  LibraryManager
+//
+//  Created by David Hein & Felix Hieser on 28.01.19.
+//  Copyright © 2019 David Hein & Felix Hieser. All rights reserved.
+//
+
+#ifndef searchUtils_h
+#define searchUtils_h
+
+#include <stdio.h>
+
+#endif /* searchUtils_h */
