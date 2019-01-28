a1c9479a3667c9d338f49e7fd40dc86be41e9d79
David Hein <david-hein196@web.de>
2019-01-28 19:01:48 +0100

    Add file fileUtils.c/h - read and write files

---
+++ LibraryManager/LibraryManager/fileUtils.c
@@ -0,0 +1,9 @@
+//
+//  fileUtils.c
+//  LibraryManager
+//
+//  Created by David Hein on 28.01.19.
+//  Copyright © 2019 David Hein. All rights reserved.
+//
+
+#include "fileUtils.h"
---
+++ LibraryManager/LibraryManager/fileUtils.h
@@ -0,0 +1,14 @@
+//
+//  fileUtils.h
+//  LibraryManager
+//
+//  Created by David Hein on 28.01.19.
+//  Copyright © 2019 David Hein. All rights reserved.
+//
+
+#ifndef fileUtils_h
+#define fileUtils_h
+
+#include <stdio.h>
+
+#endif /* fileUtils_h */
