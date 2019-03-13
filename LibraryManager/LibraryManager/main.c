8c78f5db251c50faf16dd2b5e16ef12bb4c9282d
David Hein <david-hein196@web.de>
2019-03-13 22:20:31 +0100

    First test implementation for sorting the books

--- LibraryManager/LibraryManager/main.c
+++ LibraryManager/LibraryManager/main.c
@@ -18,19 +18,46 @@
 #include <string.h>
 #include <stdlib.h>
 
+int bookCompare(const void *p1, const void *p2) {
+    if (((bookData *)p1)->sortOrder < ((bookData *)p2)->sortOrder) return 1;
+    if (((bookData *)p1)->sortOrder > ((bookData *)p2)->sortOrder) return -1;
+    return 0;
+}
+
+void sortBooks(bookData **books) {
+    qsort(books, countBooks(books), sizeof(bookData*), bookCompare);
+}
+
 int main(int argc, const char * argv[]) {
-    // Allocate memory for bookData array
     bookData **data = calloc(1, sizeof(bookData**));
-    // Read content from file
     readFile(&data, "./lib.bd", readNewLineEvent);
-    // Print content from data struct to console
-    printBookData(data);
-    // Test to write data
-    char *bookDataCSV = bookDataToCSV(data);
-    writeFile("./test.txt", bookDataCSV);
-    // Free allocated memory
+    
+    sortBooks(data);
+    
+    
+    
+    
+    printListMenu(data);
+    
+//        for (int curPos = 0; data[curPos] != NULL; curPos++)
+//            printf("%s\n", data[curPos]->title);
+    
     freeBookData(&data);
     return 0;
+    
+    
+//    // Allocate memory for bookData array
+//    bookData **data = calloc(1, sizeof(bookData**));
+//    // Read content from file
+//    readFile(&data, "./lib.bd", readNewLineEvent);
+//    // Print content from data struct to console
+//    printBookData(data);
+//    // Test to write data
+//    char *bookDataCSV = bookDataToCSV(data);
+//    writeFile("./test.txt", bookDataCSV);
+//    // Free allocated memory
+//    freeBookData(&data);
+//    return 0;
     
 //    char a = getchar();
 //    if (a == 10) printf("is 10\n");
