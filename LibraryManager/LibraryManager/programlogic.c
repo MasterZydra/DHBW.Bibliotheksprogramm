2dfafca37e59a62f70dce4a726ee49801f127d25
FHieser <felix_hieser@web.de>
2019-03-13 18:53:59 +0100

    Adding Logic for ListMenu and SelectedBookMenu

--- LibraryManager/LibraryManager/programlogic.c
+++ LibraryManager/LibraryManager/programlogic.c
@@ -14,19 +14,19 @@
 int mainMenu() {
     char input = ' ';
     bool manageMode = false;
-    
+
     while (true) {
         printMainMenu(manageMode);
-        
+
         terminalInput("%c", &input);
-        
+
         switch (input) {
             case '0': return 0;
             case '1':
                 // Search book
                 searchMenu(manageMode);
                 break;
-                
+
             default:
                 break;
         }
@@ -38,9 +38,9 @@ void searchMenu(bool manageMode) {
     char input = ' ';
     while (true) {
         printSearchMenu();
-        
+
         terminalInput("%c", &input);
-        
+
         switch (input) {
             case '0': return;
             case '1':
@@ -76,6 +76,62 @@ int searchMenuText(bool manageMode, searchCol sc) {
     terminalInput("%s", &input);
     if (strcmp(input, "0") == 0) return 1;
     // search ...
-    
+
+    return 0;
+}
+
+/**
+ List Menu Logic,
+ after printing the incoming listData you can select a book from the List for further editing.
+
+ @param listData
+ */
+
+int searchListMenu(**listData) {
+    char input = ' ';
+    bool manageMode = false;
+
+    while (true) {
+        printListMenu(&listData);
+
+        terminalInput("%c", &input);
+
+        switch (input) {
+            case '0': return 0;
+            case '1': selectedBookMenu(&listData[0]);
+            case '2': selectedBookMenu(&listData[1]);
+            case '3': selectedBookMenu(&listData[2]);
+            case '4': selectedBookMenu(&listData[3]);
+            case '5': selectedBookMenu(&listData[4]);
+            case '6': selectedBookMenu(&listData[5]);
+            case '7': selectedBookMenu(&listData[6]);
+            case '8': selectedBookMenu(&listData[7]);
+            case '9': selectedBookMenu(&listData[8]);
+
+            default:
+                break;
+        }
+    }
+    return 0;
+}
+
+
+int selectedBookMenu(bookData *selectedBook) {
+    char input = ' ';
+    bool manageMode = false;
+
+    while (true) {
+        printSelectedBookData(selectedBook);
+
+        terminalInput("%c", &input);
+
+        switch (input) {
+            case '0': return 0;
+
+
+            default:
+                break;
+        }
+    }
     return 0;
 }
